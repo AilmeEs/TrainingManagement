7ce8ed16021ec8c011a0cf4654bff6f211c07273
王帅 <2624318037.qq.com>
2019-03-20 16:48:36 +0800

    公告的删除功能 date:20190320 负责人：wangshuai

--- training/views.py
+++ training/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import DetailView, ListView
+from .models import Post
+
 
 # Create your views here.
 def index(request):
-    return render(request,'training/index.html')
+    return render(request, 'training/index.html')
+
+
+class PostList(ListView):
+    model = Post
+    template_name = 'training/post_list.html'
+
+
+def post_delete(request, id):
+    post = Post.objects.get(id=id)
+    post.delete()
+    return redirect('post_list')
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'training/post_detail.html'
